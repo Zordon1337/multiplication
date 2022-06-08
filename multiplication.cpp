469983e98a59063572e07a3760365d9d18c73cfc
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-06-08 07:43:42 +0200

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int y;
+int first;
+int nd;
+
+using namespace std;
+
+int main()
+{
+	cout << "enter first number: ";
+	cin >> first;
+	cout << "enter 2nd number: ";
+	cin >> nd;
+	y = first * nd;
+	cout << "output: " << y;
+
+}
